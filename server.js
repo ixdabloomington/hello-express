c5438417004cabebaf9c5c49ae9514d2788c8fec
Dan Hiester <spammagnet001@distantlyyours.com>
2014-09-16 12:47:10 +0000

    Demo sending parameters through the router

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser"),
     port = process.env.PORT,
     ip = process.env.IP,
     router = express.Router();
 
 router.get('/', function(req, res){
     res.send("Hello world!");
-    console.log("Ping!");
+});
+
+router.get('/hello/:name', function(req, res){
+    res.send("Hello " + req.params.name + "!")
 });
 
 app.use('/', router);
